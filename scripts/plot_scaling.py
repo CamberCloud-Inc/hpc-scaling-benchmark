107647f69edd565b19bc88c8540f85abf7f6ce38
Shane Davis <shanewdavis@gmail.com>
2025-04-04 15:48:25 -0400

    Uploaded revised plot_scaling.py script and edited README to describe its usage.

--- scripts/plot_scaling.py
+++ scripts/plot_scaling.py
@@ -1,41 +1,54 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import argparse
 
 def read_file(infile):
     ''' Read three column scaling input files'''
     data = np.loadtxt(infile)
     return data[:,0], data[:,1], data[:,2]
 
+def main(**kwargs):
+    
+    # read scaling file
+    nc, cc, pc = read_file(kwargs['infile'])
 
-# read in cpu scaling and normalize performance
-nc, cc, pc = read_file("cpu_scaling")
-y0 = np.min(pc)
-yc = pc/y0
-xc = nc
+    fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(8, 6))
 
-# read in gpu scaling and normalize performance
-ng, cg, pg = read_file("gpu_scaling")
-yg = pg/y0
-xg = ng
+    ax1.scatter(nc, pc, marker='s')
+    ax1.set_ylabel('Performance (cell updates/sec)')
 
-# Convert x-axis to cost
-cph_g = 325./24
-cph_c = 1.5*3
-xg *= cph_g
-xc *= cph_c
+    if (kwargs['annotate']):
+        for i, lab in enumerate(cc):
+            ax1.annotate(str(int(lab)), xy=(nc[i], pc[i]), xytext=(0, 6), textcoords='offset points')
+    ax1.set_yscale('log')
+    ax1.set_xscale('log')
 
-fig, ax = plt.subplots()
-
-ax.scatter(xc,yc,marker='s',label='CPU')
-ax.scatter(xg,yg,marker='o',label='GPU')
-ax.set_ylabel('Relative Performance')
-#ax.set_xlabel('Node Count')
-
-ax.set_xlabel('Cost (Camber Credits)')
-ax.set_yscale('log')
-ax.set_xscale('log')
-plt.legend()
-
-plt.savefig('scaling.png')
+    eff = 100*pc*nc[0] / (pc[0]*nc)
+    ax2.scatter(nc, eff, marker='s')
+    ax2.set_ylabel('Efficiency (%)')
+    ax2.set_xlabel('Node Count')
+    ax2.set_ylim([kwargs['emin'], kwargs['emax']])
+    ax2.set_xscale('log')
+    
+    plt.savefig(kwargs['infile'] + '.png')
 
     
+# Execute main function
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('infile',
+                        default = None,
+                        help = 'input filename')
+    parser.add_argument('-annotate',
+                        action='store_true',
+                        help = 'turn on annotations')
+    parser.add_argument('--emin',
+                        default = 95,
+                        type = float,
+                        help = 'minimum efficiency')
+    parser.add_argument('--emax',
+                        default = 105,
+                        type = float,
+                        help = 'maximum efficiency')
+    args = parser.parse_args()
+    main(**vars(args))
